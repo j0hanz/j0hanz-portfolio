9f9ac2a0e68d866503f94a5d7c4f3abe132a1d2c
Linus Johansson <golfarenlj@gmail.com>
2024-08-09 17:31:06 +0000

    Update Hero component with image and styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import Image from '../assets/image_me.jpeg';
+import styles from './styles/Hero.module.css';
 
 const Hero = () => (
   <section id="hero" className="hero-section text-light text-center py-5">
-    <Container>
-      <Row className="d-flex justify-content-center text-center align-items-center">
-        <Col md={8}>
-          <h1>Hello, I'm Linus Johansson</h1>
-          <h2>Frontend Developer | Backend Developer | Web Developer</h2>
-          <p className="lead">Crafting elegant and efficient web solutions.</p>
-          <Button variant="primary" href="#contact">
+    <Container className="px-0">
+      <Row className="d-flex justify-content-center text-center align-items-center mx-auto">
+        <Col xs="auto">
+          <h1 className="display-4 mb-3">
+            <span className={styles.gradientText}>Hello,</span> I'm Linus
+            Johansson
+          </h1>
+          <img src={Image} alt="Linus Johansson" className={styles.heroImage} />
+          <h2 className="h4 mb-3">Full-Stack Developer</h2>
+          <p className="lead mb-4">
+            Crafting elegant and efficient web solutions.
+          </p>
+          <Button
+            variant="outline-success text-white"
+            href="#contact"
+            size="lg"
+          >
             Get in Touch
           </Button>
         </Col>
