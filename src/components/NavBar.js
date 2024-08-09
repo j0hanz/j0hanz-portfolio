59098a89af385a4679fbc69bda03f2a77dbf6e18
Linus Johansson <golfarenlj@gmail.com>
2024-08-09 17:31:27 +0000

    Add FontAwesome icons to NavBar component for site navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import {
+  faUser,
+  faCogs,
+  faBriefcase,
+  faProjectDiagram,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => (
   <Navbar fixed="top" bg="dark" variant="dark" expand="lg" className="py-1">
@@ -12,11 +19,26 @@ const NavBar = () => (
       <Navbar.Toggle aria-controls="navbar-nav" />
       <Navbar.Collapse id="navbar-nav">
         <Nav className="ms-auto">
-          <Nav.Link href="#about-me">About Me</Nav.Link>
-          <Nav.Link href="#skills">Skills</Nav.Link>
-          <Nav.Link href="#work-experience">Experience</Nav.Link>
-          <Nav.Link href="#portfolio">Projects</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav.Link href="#about-me">
+            <FontAwesomeIcon icon={faUser} className="me-2" />
+            About Me
+          </Nav.Link>
+          <Nav.Link href="#skills">
+            <FontAwesomeIcon icon={faCogs} className="me-2" />
+            Skills
+          </Nav.Link>
+          <Nav.Link href="#work-experience">
+            <FontAwesomeIcon icon={faBriefcase} className="me-2" />
+            Experience
+          </Nav.Link>
+          <Nav.Link href="#portfolio">
+            <FontAwesomeIcon icon={faProjectDiagram} className="me-2" />
+            Projects
+          </Nav.Link>
+          <Nav.Link href="#contact">
+            <FontAwesomeIcon icon={faEnvelope} className="me-2" />
+            Contact
+          </Nav.Link>
         </Nav>
         <Nav className="mr-auto">
           <Nav.Link
