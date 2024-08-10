560a309181c2d07b7473bf9ced7bef687dbe23b8
Linus Johansson <golfarenlj@gmail.com>
2024-08-10 12:40:46 +0000

    Add useCallback to handleClickOutside in useClickOutsideToggle hook

--- src/hooks/OutsideClickHandler.js
+++ src/hooks/OutsideClickHandler.js
@@ -1,27 +1,29 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 
 const useClickOutsideToggle = ({ ignoreRefs = [] } = {}) => {
   const [expanded, setExpanded] = useState(false);
   const ref = useRef(null);
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      const isOutsideClick =
-        !ref.current?.contains(event.target) &&
-        ignoreRefs.every(
-          (ignoreRef) => !ignoreRef.current?.contains(event.target),
-        );
-
-      if (isOutsideClick) {
+  const handleClickOutside = useCallback(
+    (event) => {
+      if (
+        ref.current &&
+        !ref.current.contains(event.target) &&
+        !ignoreRefs.some((ignoreRef) =>
+          ignoreRef.current?.contains(event.target),
+        )
+      ) {
         setExpanded(false);
       }
-    };
+    },
+    [ignoreRefs],
+  );
 
+  useEffect(() => {
     document.addEventListener('mouseup', handleClickOutside);
     return () => {
       document.removeEventListener('mouseup', handleClickOutside);
     };
-  }, [ignoreRefs]);
+  }, [handleClickOutside]);
 
   return { expanded, setExpanded, ref };
 };
