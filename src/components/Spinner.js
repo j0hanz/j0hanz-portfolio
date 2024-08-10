f4c2152e9b2bc00becd8362aa4ed0d20f8df2f77
Linus Johansson <golfarenlj@gmail.com>
2024-08-10 11:07:48 +0000

    Add Spinner component for loading state visualization

---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './styles/Spinner.module.css';
+
+const Spinner = () => {
+  return (
+    <div className={styles.spinner}>
+      <div className={styles.bounce}></div>
+      <div className={styles.bounce}></div>
+    </div>
+  );
+};
+
+export default Spinner;
