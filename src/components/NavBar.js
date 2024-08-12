ae030ab758564aca75ed202c8f01669ea781b53d
Linus Johansson <golfarenlj@gmail.com>
2024-08-12 12:15:43 +0000

    Update NavBar module to use CSS module for consistent styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
-import React from 'react'; // Importing React library
-import { Navbar, Nav, Container } from 'react-bootstrap'; // Importing components from react-bootstrap
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Importing FontAwesomeIcon component
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'; // Importing specific icons from FontAwesome
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import {
   faUser,
   faCogs,
@@ -10,34 +10,37 @@ import {
   faEnvelope,
   faGraduationCap,
   faFilePdf,
-} from '@fortawesome/free-solid-svg-icons'; // Importing specific icons from FontAwesome
-import useClickOutsideToggle from '../hooks/OutsideClickHandler'; // Importing custom hook for handling outside click
-import styles from './styles/NavBar.module.css'; // Importing CSS module for styling
-import Cv from '../assets/Linus_Johansson_CV.pdf'; // Importing the PDF file
+  faBars,
+} from '@fortawesome/free-solid-svg-icons';
+import useClickOutsideToggle from '../hooks/OutsideClickHandler';
+import styles from './styles/NavBar.module.css';
+import Cv from '../assets/Linus_Johansson_CV.pdf';
 
-// Functional component NavBar
 const NavBar = () => {
-  const { expanded, setExpanded, ref } = useClickOutsideToggle(); // Destructuring expanded state, setExpanded function, and ref from custom hook
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   return (
     <Navbar
-      ref={ref} // Reference for detecting outside clicks
-      fixed="top" // Fixing the navbar at the top
-      bg="dark" // Setting background color to dark
-      variant="dark" // Setting variant to dark
-      expand="lg" // Enabling navbar to expand on large screens
-      className="py-1" // Adding padding to the navbar
-      expanded={expanded} // Controlling the expanded state of the navbar
+      ref={ref}
+      fixed="top"
+      bg="dark"
+      variant="dark"
+      expand="lg"
+      className={`${styles.customNavbar} py-1`}
+      expanded={expanded}
     >
       <Container fluid>
-        <Navbar.Brand>
+        <Navbar.Brand className={styles.navbarBrand}>
           <span className="text-light">Linus Johansson</span>
         </Navbar.Brand>
         <Navbar.Toggle
-          aria-controls="navbar-nav" // ARIA attribute for accessibility
-          onClick={() => setExpanded(!expanded)} // Toggling the expanded state on click
-        />
-        <Navbar.Collapse id="navbar-nav">
+          aria-controls="navbar-nav"
+          onClick={() => setExpanded(!expanded)}
+          className={styles.customToggle}
+        >
+          <FontAwesomeIcon icon={faBars} size="lg" className={styles.navIcon} />
+        </Navbar.Toggle>
+        <Navbar.Collapse id="navbar-nav" className={styles.navbarCollapse}>
           <Nav className="ms-auto">
             {[
               { id: 'about-me', icon: faUser, label: 'About Me' },
@@ -47,18 +50,18 @@ const NavBar = () => {
               { id: 'portfolio', icon: faProjectDiagram, label: 'Projects' },
               { id: 'contact', icon: faEnvelope, label: 'Contact' },
             ].map(({ id, icon, label }) => (
-              <Nav.Link key={id} href={`#${id}`}>
+              <Nav.Link key={id} href={`#${id}`} className={styles.navLink}>
                 <FontAwesomeIcon icon={icon} className={styles.navIcon} />{' '}
                 {label}
               </Nav.Link>
             ))}
           </Nav>
-          <Nav className="mr-auto">
+          <Nav className="ml-auto">
             {[
               {
-                id: 'linkedin', // ID for the link
-                icon: faLinkedin, // Icon for the link
-                href: 'https://www.linkedin.com/in/linus-johansson-9b1a302a0/', // URL for the link
+                id: 'linkedin',
+                icon: faLinkedin,
+                href: 'https://www.linkedin.com/in/linus-johansson-9b1a302a0/',
               },
               {
                 id: 'github',
@@ -67,20 +70,21 @@ const NavBar = () => {
               },
               {
                 id: 'download-pdf',
-                icon: faFilePdf, // Icon for the PDF download
-                href: Cv, // Use the imported Cv directly as href
-                download: true, // Ensures the file is downloaded
+                icon: faFilePdf,
+                href: Cv,
+                download: true,
+                label: 'CV',
               },
             ].map(({ id, icon, href, download, label }) => (
               <Nav.Link
                 key={id}
                 href={href}
-                target={download ? '_self' : '_blank'} // Open in the same tab if downloading
-                className="text-light" // Setting text color to light
-                download={download} // Adding download attribute if necessary
+                target={download ? '_self' : '_blank'}
+                className={styles.navLink}
+                download={download}
               >
-                <FontAwesomeIcon icon={icon} size="lg" /> {/* Icon */}
-                {label && <span className="ms-2">{label}</span>} {/* Label */}
+                <FontAwesomeIcon icon={icon} size="lg" className="me-2" />
+                {label && <span>{label}</span>}
               </Nav.Link>
             ))}
           </Nav>
@@ -90,5 +94,4 @@ const NavBar = () => {
   );
 };
 
-// Exporting the NavBar component as default export
 export default NavBar;
