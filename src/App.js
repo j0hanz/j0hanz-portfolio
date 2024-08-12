4d885a3745a5f070290bf341393cafbd310f043a
Linus Johansson <golfarenlj@gmail.com>
2024-08-12 13:59:21 +0000

    Update App.js to include lazy loading for images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
@@ -9,34 +9,50 @@ import Education from './components/Education';
 import Portfolio from './components/Portfolio';
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
-import SuccessMessage from './components/SuccessMessage'; // Import the SuccessMessage component
+import SuccessMessage from './components/SuccessMessage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './App.module.css';
+import Spinner from './components/Spinner';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading period
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Router>
       <div className={styles.appContainer}>
         <NavBar />
         <main className={styles.mainContent}>
-          <Routes>
-            <Route path="/success" element={<SuccessMessage />} />
-            <Route
-              path="/"
-              element={
-                <>
-                  <Hero />
-                  <AboutMe />
-                  <Education />
-                  <Skills />
-                  <WorkExperience />
-                  <Portfolio />
-                  <ContactForm />
-                  <Footer />
-                </>
-              }
-            />
-          </Routes>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <Routes>
+              <Route path="/success" element={<SuccessMessage />} />
+              <Route
+                path="/"
+                element={
+                  <>
+                    <Hero />
+                    <AboutMe />
+                    <Education />
+                    <Skills />
+                    <WorkExperience />
+                    <Portfolio />
+                    <ContactForm />
+                    <Footer />
+                  </>
+                }
+              />
+            </Routes>
+          )}
         </main>
       </div>
     </Router>
