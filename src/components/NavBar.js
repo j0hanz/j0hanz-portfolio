5e36b910e656acf4c9495fe9f125f237c3dd6296
Linus Johansson <golfarenlj@gmail.com>
2024-08-12 10:39:24 +0000

    Update NavBar module to use CSS module for consistent styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,17 +9,17 @@ import {
   faProjectDiagram,
   faEnvelope,
   faGraduationCap,
+  faFilePdf,
 } from '@fortawesome/free-solid-svg-icons'; // Importing specific icons from FontAwesome
 import useClickOutsideToggle from '../hooks/OutsideClickHandler'; // Importing custom hook for handling outside click
 import styles from './styles/NavBar.module.css'; // Importing CSS module for styling
+import Cv from '../assets/Linus_Johansson_CV.pdf'; // Importing the PDF file
 
 // Functional component NavBar
 const NavBar = () => {
-  // Destructuring expanded state, setExpanded function, and ref from custom hook
-  const { expanded, setExpanded, ref } = useClickOutsideToggle();
+  const { expanded, setExpanded, ref } = useClickOutsideToggle(); // Destructuring expanded state, setExpanded function, and ref from custom hook
 
   return (
-    // Navbar component from react-bootstrap with various props for styling and behavior
     <Navbar
       ref={ref} // Reference for detecting outside clicks
       fixed="top" // Fixing the navbar at the top
@@ -29,65 +29,31 @@ const NavBar = () => {
       className="py-1" // Adding padding to the navbar
       expanded={expanded} // Controlling the expanded state of the navbar
     >
-      {/* Bootstrap container with fluid width */}
       <Container fluid>
-        {/* Navbar brand with text */}
         <Navbar.Brand>
           <span className="text-light">Linus Johansson</span>
         </Navbar.Brand>
-        {/* Navbar toggle button for collapsing/expanding the navbar */}
         <Navbar.Toggle
           aria-controls="navbar-nav" // ARIA attribute for accessibility
           onClick={() => setExpanded(!expanded)} // Toggling the expanded state on click
         />
-        {/* Navbar collapse component for collapsible content */}
         <Navbar.Collapse id="navbar-nav">
-          {/* Bootstrap nav component aligned to the right */}
           <Nav className="ms-auto">
-            {/* Mapping through an array of navigation items */}
             {[
-              {
-                id: 'about-me', // ID for the section
-                icon: faUser, // Icon for the section
-                label: 'About Me', // Label for the section
-              },
-              {
-                id: 'education',
-                icon: faGraduationCap,
-                label: 'Education',
-              },
-              {
-                id: 'skills',
-                icon: faCogs,
-                label: 'Skills',
-              },
-              {
-                id: 'work-experience',
-                icon: faBriefcase,
-                label: 'Experience',
-              },
-              {
-                id: 'portfolio',
-                icon: faProjectDiagram,
-                label: 'Projects',
-              },
-              {
-                id: 'contact',
-                icon: faEnvelope,
-                label: 'Contact',
-              },
+              { id: 'about-me', icon: faUser, label: 'About Me' },
+              { id: 'education', icon: faGraduationCap, label: 'Education' },
+              { id: 'skills', icon: faCogs, label: 'Skills' },
+              { id: 'work-experience', icon: faBriefcase, label: 'Experience' },
+              { id: 'portfolio', icon: faProjectDiagram, label: 'Projects' },
+              { id: 'contact', icon: faEnvelope, label: 'Contact' },
             ].map(({ id, icon, label }) => (
-              // Nav link for each navigation item
               <Nav.Link key={id} href={`#${id}`}>
                 <FontAwesomeIcon icon={icon} className={styles.navIcon} />{' '}
-                {/* Icon */}
-                {label} {/* Label */}
+                {label}
               </Nav.Link>
             ))}
           </Nav>
-          {/* Bootstrap nav component aligned to the left */}
           <Nav className="mr-auto">
-            {/* Mapping through an array of social media links */}
             {[
               {
                 id: 'linkedin', // ID for the link
@@ -99,15 +65,22 @@ const NavBar = () => {
                 icon: faGithub,
                 href: 'https://github.com/j0hanz',
               },
-            ].map(({ id, icon, href }) => (
-              // Nav link for each social media link
+              {
+                id: 'download-pdf',
+                icon: faFilePdf, // Icon for the PDF download
+                href: Cv, // Use the imported Cv directly as href
+                download: true, // Ensures the file is downloaded
+              },
+            ].map(({ id, icon, href, download, label }) => (
               <Nav.Link
                 key={id}
                 href={href}
-                target="_blank" // Opening the link in a new tab
+                target={download ? '_self' : '_blank'} // Open in the same tab if downloading
                 className="text-light" // Setting text color to light
+                download={download} // Adding download attribute if necessary
               >
                 <FontAwesomeIcon icon={icon} size="lg" /> {/* Icon */}
+                {label && <span className="ms-2">{label}</span>} {/* Label */}
               </Nav.Link>
             ))}
           </Nav>
