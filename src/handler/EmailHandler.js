00778a0109ed34b789acbf7bdd6c713a17cbf747
Linus Johansson <golfarenlj@gmail.com>
2024-08-14 18:08:40 +0000

    Update EmailHandler component with useEffect hook for sending email on component mount

--- src/handler/EmailHandler.js
+++ src/handler/EmailHandler.js
@@ -1,28 +1,33 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import emailjs from 'emailjs-com';
 
 const EmailHandler = ({ formData, onEmailSent }) => {
   const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    const sendEmail = () => {
+      emailjs
+        .sendForm(
+          'service_h7mpv2i',
+          'template_mnrvg75',
+          form.current,
+          'kMJNphEQu6FYZEE90',
+        )
+        .then(
+          (result) => {
+            onEmailSent(true); // Notify parent component of success
+          },
+          (error) => {
+            onEmailSent(false); // Notify parent component of failure
+          },
+        );
+    };
 
-    emailjs
-      .sendForm('service_h7mpv2i', 'template_mnrvg75', form.current, 'kMJNphEQu6FYZEE90')
-      .then(
-        (result) => {
-          console.log('Email sent successfully!', result.text);
-          onEmailSent(true); // Callback to notify parent component of success
-        },
-        (error) => {
-          console.error('Failed to send email.', error.text);
-          onEmailSent(false); // Callback to notify parent component of failure
-        }
-      );
-  };
+    sendEmail(); // Automatically send email when component mounts
+  }, [formData, onEmailSent]);
 
   return (
-    <form ref={form} onSubmit={sendEmail} style={{ display: 'none' }}>
+    <form ref={form} style={{ display: 'none' }}>
       <input type="hidden" name="to_email" value="l.johansson93@outlook.com" />
       <input type="hidden" name="from_name" value={formData.name} />
       <input type="hidden" name="from_email" value={formData.email} />
