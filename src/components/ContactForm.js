39b1ce49256c477d1ebdd070e95d35d03d26021b
Linus Johansson <golfarenlj@gmail.com>
2024-08-14 18:08:34 +0000

    Update ContactForm component with improved form submission functionality and error handling

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,23 +1,22 @@
-import React, { useState } from 'react'; // Importing React and useState hook
-import { useNavigate } from 'react-router-dom'; // Importing useNavigate hook
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'; // Importing Bootstrap components
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Importing FontAwesomeIcon component
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; // Importing FontAwesome icon
-import EmailHandler from '../handler/EmailHandler'; // Importing EmailHandler component
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import EmailHandler from '../handler/EmailHandler';
 
-// Functional component ContactForm
-const ContactForm = React.memo(() => {
-  const navigate = useNavigate(); // Initializing useNavigate hook
-  const [validated, setValidated] = useState(false); // State for form validation
+const ContactForm = () => {
+  const navigate = useNavigate();
+  const [validated, setValidated] = useState(false);
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     message: '',
-  }); // State for form data
+  });
 
-  // Handle form submission
   const handleSubmit = (event) => {
     const form = event.currentTarget;
+
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
@@ -27,39 +26,30 @@ const ContactForm = React.memo(() => {
     }
   };
 
-  // Handle form data change
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  // Handle email sent success or failure
   const handleEmailSent = (success) => {
     if (success) {
       setFormData({ name: '', email: '', message: '' }); // Reset form fields
-      navigate('/success'); // Navigate to success page on email success
+      navigate('/success'); // Navigate to success page
     } else {
-      console.error('Email sending failed.'); // Handle email sending failure
+      console.error('Email sending failed.');
     }
   };
 
   return (
-    // Section element with id and classes for styling
     <section id="contact" className="contact-section py-5">
-      {/* Bootstrap container with no horizontal padding */}
       <Container className="px-0">
-        {/* Heading with FontAwesome icon and centered text */}
         <h2 className="text-center">
           <FontAwesomeIcon icon={faEnvelope} size="sm" className="me-2" />
           Contact
         </h2>
-        {/* Bootstrap row centered horizontally with top margin */}
         <Row className="justify-content-center mt-4 mx-auto">
-          {/* Bootstrap column with medium size 8 */}
           <Col md={8}>
-            {/* Bootstrap form */}
             <Form noValidate validated={validated} onSubmit={handleSubmit}>
-              {/* Form group for name input */}
               <Form.Group controlId="formName">
                 <Form.Label className="d-none">Name</Form.Label>
                 <Form.Control
@@ -72,7 +62,6 @@ const ContactForm = React.memo(() => {
                   onChange={handleChange}
                 />
               </Form.Group>
-              {/* Form group for email input */}
               <Form.Group controlId="formEmail">
                 <Form.Label className="d-none">Email</Form.Label>
                 <Form.Control
@@ -85,7 +74,6 @@ const ContactForm = React.memo(() => {
                   onChange={handleChange}
                 />
               </Form.Group>
-              {/* Form group for message textarea */}
               <Form.Group controlId="formMessage">
                 <Form.Label className="d-none">Message</Form.Label>
                 <Form.Control
@@ -99,7 +87,6 @@ const ContactForm = React.memo(() => {
                   onChange={handleChange}
                 />
               </Form.Group>
-              {/* Submit button with outline-primary variant */}
               <div className="d-flex justify-content-center">
                 <Button variant="outline-primary d-flex" type="submit">
                   Send
@@ -109,13 +96,11 @@ const ContactForm = React.memo(() => {
           </Col>
         </Row>
       </Container>
-      {/* Integrate EmailHandler component */}
       {validated && (
         <EmailHandler formData={formData} onEmailSent={handleEmailSent} />
       )}
     </section>
   );
-});
+};
 
-// Exporting the ContactForm component as default export
 export default ContactForm;
