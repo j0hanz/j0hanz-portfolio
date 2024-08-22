62194ef665ba4a72691aeebe5f504e5a97eac46f
Linus Johansson <golfarenlj@gmail.com>
2024-08-22 20:38:31 +0000

    Improve Hero component code structure and download functionality

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,58 +7,45 @@ import { faDownload, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Cv from '../assets/Linus_Johansson_CV.pdf';
 
 const Hero = React.memo(() => {
-  const [loading, setLoading] = useState(false); // State to manage loading spinner
+  const [loading, setLoading] = useState(false);
 
-  // Function to handle the CV download process
   const handleDownload = () => {
-    setLoading(true); // Set loading to true when download starts
+    setLoading(true);
 
-    // Create a temporary link element for downloading the CV
     const link = document.createElement('a');
-    link.href = Cv; // Set the href to the CV file path
-    link.download = 'Linus_Johansson_CV.pdf'; // Set the download attribute with the file name
-    document.body.appendChild(link); // Append the link to the document body
-    link.click(); // Programmatically trigger the click event to start the download
-    document.body.removeChild(link); // Remove the link from the document after the download starts
+    link.href = Cv;
+    link.download = 'Linus_Johansson_CV.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 
-    // Ensure the spinner is shown for at least 2 seconds
-    setTimeout(() => {
-      setLoading(false); // Set loading to false after 2 seconds
-    }, 2000);
+    setTimeout(() => setLoading(false), 2000);
   };
 
-  // CSS class combinations for various elements
-  const classes = `mb-4 ${styles.gradientText}`;
-  const imageClasses = `mb-4 ${styles.heroImage}`;
-  const leadClasses = `lead`;
-
   return (
-    <section id="hero" className="hero-section text-center py-4">
-      <Container className="px-0">
-        <Row className="d-flex justify-content-center text-center align-items-center mx-auto">
+    <section id="hero" className="text-center py-4">
+      <Container>
+        <Row className="d-flex justify-content-center align-items-center">
           <Col xs="auto">
-            {/* Hero Title */}
-            <h1 className={classes}>Linus Johansson</h1>
-            {/* Hero Image */}
-            <img src={Image} alt="Linus Johansson" className={imageClasses} />
-            {/* Subtitle */}
+            <h1 className={`mb-4 ${styles.gradientText}`}>Linus Johansson</h1>
+            <img
+              src={Image}
+              alt="Linus Johansson"
+              className={`mb-4 ${styles.heroImage}`}
+            />
             <h2>Full-Stack Developer</h2>
-            {/* Divider Line */}
             <hr className="my-2" />
-            {/* Lead Paragraph */}
-            <p className={leadClasses}>
-              Creating elegant and efficient web solutions.
+            <p className="lead">
+              Crafting elegant and efficient web solutions.
             </p>
-            {/* Download CV Button and Contact Button */}
-            <div className="d-flex flex-column justify-content-center align-items-center my-3">
+            <div className="d-flex flex-column align-items-center my-3">
               <Button
                 onClick={handleDownload}
-                variant="outline-primary mb-3 "
+                variant="outline-primary"
                 disabled={loading}
-                className={styles.customButton}
+                className={`mb-3 ${styles.customButton}`}
               >
                 {loading ? (
-                  // Show spinner and "Downloading..." text while loading
                   <>
                     <Spinner
                       as="span"
@@ -71,18 +58,15 @@ const Hero = React.memo(() => {
                     Downloading...
                   </>
                 ) : (
-                  // Show the normal download Button with icon when not loading
                   <>
                     <FontAwesomeIcon
                       icon={faDownload}
                       className={styles.buttonIcon}
                     />
-                    <span className={styles.buttonText}>Download CV</span>
+                    Download CV
                   </>
                 )}
               </Button>
-
-              {/* Contact Button */}
               <Button
                 onClick={() => (window.location.href = '#contact')}
                 variant="outline-dark"
@@ -92,7 +76,7 @@ const Hero = React.memo(() => {
                   icon={faEnvelope}
                   className={styles.buttonIcon}
                 />
-                <span className={styles.buttonText}>Get in Touch</span>
+                Get in Touch
               </Button>
             </div>
           </Col>
