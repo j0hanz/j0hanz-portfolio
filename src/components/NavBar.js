0e7327a7a1a33f4692cab8ffdf426a82669a1edb
Linus Johansson <golfarenlj@gmail.com>
2024-09-10 15:47:54 +0000

    Refactor code to add CV modal functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Navbar,
   Nav,
@@ -20,99 +20,109 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import useClickOutsideToggle from '../hooks/OutsideClickHandler';
 import styles from './styles/NavBar.module.css';
-import Cv from '../assets/Linus_Johansson_CV.pdf';
+import ModalCv from './ModalCv';
 
 const NavBar = () => {
   const { expanded, setExpanded, ref } = useClickOutsideToggle();
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalOpen = () => setShowModal(true);
+  const handleModalClose = () => setShowModal(false);
 
   return (
-    <Navbar
-      ref={ref}
-      fixed="top"
-      bg="dark"
-      variant="dark"
-      expand="lg"
-      className={`${styles.customNavbar} py-1`}
-      expanded={expanded}
-    >
-      <Container fluid className="px-2">
-        <Navbar.Brand className={styles.navbarBrand}>
-          <Nav.Link href="#hero" className={styles.nameNav}>
-            Linus Johansson
-          </Nav.Link>
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="navbar-nav"
-          onClick={() => setExpanded(!expanded)}
-          className={styles.customToggle}
-        >
-          <FontAwesomeIcon icon={faBars} className={styles.navToggle} />
-        </Navbar.Toggle>
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="ms-auto my-1 my-lg-0">
-            {[
-              { id: 'about-me', icon: faUser, label: 'About Me' },
-              { id: 'education', icon: faGraduationCap, label: 'Education' },
-              { id: 'skills', icon: faCogs, label: 'Skills' },
-              { id: 'work-experience', icon: faBriefcase, label: 'Experience' },
-              { id: 'portfolio', icon: faProjectDiagram, label: 'Projects' },
-              { id: 'contact', icon: faEnvelope, label: 'Contact' },
-            ].map(({ id, icon, label }) => (
-              <Nav.Link
-                key={id}
-                href={`#${id}`}
-                className={`${styles.navLink} my-1 my-lg-0`}
-              >
-                <FontAwesomeIcon
-                  icon={icon}
-                  size="sm"
-                  className={`${styles.navIcon} me-1 me-lg-0 ms-1 ms-lg-0`}
-                />
-                {label}
-              </Nav.Link>
-            ))}
-          </Nav>
-          <Nav className="d-flex flex-row justify-content-start">
-            {[
-              {
-                id: 'linkedin',
-                icon: faLinkedin,
-                href: 'https://www.linkedin.com/in/linus-johansson-software-dev/',
-                tooltip: 'LinkedIn Profile',
-              },
-              {
-                id: 'github',
-                icon: faGithub,
-                href: 'https://github.com/j0hanz',
-                tooltip: 'GitHub Profile',
-              },
-              {
-                id: 'download-pdf',
-                icon: faFilePdf,
-                href: Cv,
-                download: true,
-                tooltip: 'Download CV',
-              },
-            ].map(({ id, icon, href, download, tooltip }) => (
-              <OverlayTrigger
-                key={id}
-                placement="bottom"
-                overlay={<Tooltip id={`tooltip-${id}`}>{tooltip}</Tooltip>}
-              >
+    <>
+      <Navbar
+        ref={ref}
+        fixed="top"
+        bg="dark"
+        variant="dark"
+        expand="lg"
+        className={`${styles.customNavbar} py-1`}
+        expanded={expanded}
+      >
+        <Container fluid className="px-2">
+          <Navbar.Brand className={styles.navbarBrand}>
+            <Nav.Link href="#hero" className={styles.nameNav}>
+              Linus Johansson
+            </Nav.Link>
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="navbar-nav"
+            onClick={() => setExpanded(!expanded)}
+            className={styles.customToggle}
+          >
+            <FontAwesomeIcon icon={faBars} className={styles.navToggle} />
+          </Navbar.Toggle>
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="ms-auto my-1 my-lg-0">
+              {[
+                { id: 'about-me', icon: faUser, label: 'About Me' },
+                { id: 'education', icon: faGraduationCap, label: 'Education' },
+                { id: 'skills', icon: faCogs, label: 'Skills' },
+                {
+                  id: 'work-experience',
+                  icon: faBriefcase,
+                  label: 'Experience',
+                },
+                { id: 'portfolio', icon: faProjectDiagram, label: 'Projects' },
+                { id: 'contact', icon: faEnvelope, label: 'Contact' },
+              ].map(({ id, icon, label }) => (
                 <Nav.Link
-                  href={href}
-                  target={download ? '_self' : '_blank'}
-                  className="mx-0 mx-lg-0 me-5 me-lg-0 mt-2 mt-lg-0"
-                  download={download}
+                  key={id}
+                  href={`#${id}`}
+                  className={`${styles.navLink} my-1 my-lg-0`}
                 >
-                  <FontAwesomeIcon icon={icon} size="lg" />
+                  <FontAwesomeIcon
+                    icon={icon}
+                    size="sm"
+                    className={`${styles.navIcon} me-1 me-lg-0 ms-1 ms-lg-0`}
+                  />
+                  {label}
                 </Nav.Link>
-              </OverlayTrigger>
-            ))}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+              ))}
+            </Nav>
+            <Nav className="d-flex flex-row justify-content-start">
+              {[
+                {
+                  id: 'linkedin',
+                  icon: faLinkedin,
+                  href: 'https://www.linkedin.com/in/linus-johansson-software-dev/',
+                  tooltip: 'LinkedIn Profile',
+                },
+                {
+                  id: 'github',
+                  icon: faGithub,
+                  href: 'https://github.com/j0hanz',
+                  tooltip: 'GitHub Profile',
+                },
+                {
+                  id: 'download-pdf',
+                  icon: faFilePdf,
+                  onClick: handleModalOpen,
+                  tooltip: 'Download CV',
+                },
+              ].map(({ id, icon, href, onClick, tooltip }) => (
+                <OverlayTrigger
+                  key={id}
+                  placement="bottom"
+                  overlay={<Tooltip id={`tooltip-${id}`}>{tooltip}</Tooltip>}
+                >
+                  <Nav.Link
+                    href={href}
+                    onClick={onClick}
+                    target={href ? '_blank' : undefined}
+                    className="mx-0 mx-lg-0 me-5 me-lg-0 mt-2 mt-lg-0"
+                  >
+                    <FontAwesomeIcon icon={icon} size="lg" />
+                  </Nav.Link>
+                </OverlayTrigger>
+              ))}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <ModalCv show={showModal} handleClose={handleModalClose} />
+    </>
   );
 };
 
