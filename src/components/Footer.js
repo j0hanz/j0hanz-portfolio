003126a5eb857a334d6dafaa35ed29b8c7cab3fa
Linus Johansson <golfarenlj@gmail.com>
2024-09-10 15:47:19 +0000

    Refactor code to add CV modal functionality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -13,129 +13,139 @@ import {
   faCode,
   faAward,
 } from '@fortawesome/free-solid-svg-icons';
-import Cv from '../assets/Linus_Johansson_CV.pdf';
 import styles from './styles/Footer.module.css';
+import ModalCv from './ModalCv';
 
-const Footer = () => (
-  <footer className="py-3 bg-dark text-light">
-    <Container fluid className="px-2">
-      <Row className="mx-auto">
-        <Col md={6} className="mb-md-0">
-          <h5>Contact Details</h5>
-          <p>
-            <FontAwesomeIcon
-              icon={faEnvelope}
-              size="sm"
-              className={styles.footerIcon}
-            />
-            <a
-              href="mailto:l.johansson93@outlook.com"
-              className="text-light text-decoration-none"
-            >
-              l.johansson93@outlook.com
-            </a>
-          </p>
-          <p>
-            <FontAwesomeIcon
-              icon={faPhone}
-              size="sm"
-              className={styles.footerIcon}
-            />
-            <a
-              href="tel:+46705292674"
-              className="text-light text-decoration-none"
-            >
-              +46 70 529 26 74
-            </a>
-          </p>
-        </Col>
-        <Col md={6} className="text-md-end my-1">
-          <p className="d-flex align-items-center justify-content-md-end">
-            <span className={styles.copyrightText}>
+const Footer = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalOpen = () => setShowModal(true);
+  const handleModalClose = () => setShowModal(false);
+
+  return (
+    <footer className="py-3 bg-dark text-light">
+      <Container fluid className="px-2">
+        <Row className="mx-auto">
+          <Col md={6} className="mb-md-0">
+            <h5>Contact Details</h5>
+            <p>
               <FontAwesomeIcon
-                icon={faCreativeCommons}
+                icon={faEnvelope}
+                size="sm"
                 className={styles.footerIcon}
               />
-              2024 Linus Johansson
-            </span>
-          </p>
-          <div>
-            <OverlayTrigger
-              placement="bottom"
-              overlay={
-                <Tooltip id="tooltip-linkedin">LinkedIn Profile</Tooltip>
-              }
-            >
               <a
-                className="text-light me-4"
-                href="https://www.linkedin.com/in/linus-johansson-software-dev/"
-                rel="noopener noreferrer"
-                target="_blank"
-                aria-label="LinkedIn Profile"
+                href="mailto:l.johansson93@outlook.com"
+                className="text-light text-decoration-none"
               >
-                <FontAwesomeIcon icon={faLinkedin} size="lg" />
+                l.johansson93@outlook.com
               </a>
-            </OverlayTrigger>
-            <OverlayTrigger
-              placement="bottom"
-              overlay={<Tooltip id="tooltip-github">GitHub Profile</Tooltip>}
-            >
+            </p>
+            <p>
+              <FontAwesomeIcon
+                icon={faPhone}
+                size="sm"
+                className={styles.footerIcon}
+              />
               <a
-                className="text-light me-4"
-                href="https://github.com/j0hanz"
-                rel="noopener noreferrer"
-                target="_blank"
-                aria-label="GitHub Profile"
+                href="tel:+46705292674"
+                className="text-light text-decoration-none"
               >
-                <FontAwesomeIcon icon={faGithub} size="lg" />
+                +46 70 529 26 74
               </a>
-            </OverlayTrigger>
-            <OverlayTrigger
-              placement="bottom"
-              overlay={<Tooltip id="tooltip-pdf">Download CV</Tooltip>}
-            >
-              <a
-                className="text-light me-4"
-                href={Cv}
-                download={true}
-                aria-label="Download CV"
+            </p>
+          </Col>
+          <Col md={6} className="text-md-end my-1">
+            <p className="d-flex align-items-center justify-content-md-end">
+              <span className={styles.copyrightText}>
+                <FontAwesomeIcon
+                  icon={faCreativeCommons}
+                  className={styles.footerIcon}
+                />
+                2024 Linus Johansson
+              </span>
+            </p>
+            <div>
+              <OverlayTrigger
+                placement="bottom"
+                overlay={
+                  <Tooltip id="tooltip-linkedin">LinkedIn Profile</Tooltip>
+                }
               >
-                <FontAwesomeIcon icon={faFilePdf} size="lg" />
-              </a>
-            </OverlayTrigger>
-            <OverlayTrigger
-              placement="bottom"
-              overlay={<Tooltip id="tooltip-certificate">Certificate</Tooltip>}
-            >
-              <a
-                className="text-light me-4"
-                href="https://www.credential.net/dd705ce7-f66c-456a-b07d-e8712cd7287c#gs.cubcle"
-                rel="noopener noreferrer"
-                target="_blank"
-                aria-label="Certificate"
+                <a
+                  className="text-light me-4"
+                  href="https://www.linkedin.com/in/linus-johansson-software-dev/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  aria-label="LinkedIn Profile"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} size="lg" />
+                </a>
+              </OverlayTrigger>
+              <OverlayTrigger
+                placement="bottom"
+                overlay={<Tooltip id="tooltip-github">GitHub Profile</Tooltip>}
               >
-                <FontAwesomeIcon icon={faAward} size="lg" />
-              </a>
-            </OverlayTrigger>
-            <OverlayTrigger
-              placement="bottom"
-              overlay={<Tooltip id="tooltip-code">Source Code</Tooltip>}
-            >
-              <a
-                className="text-light"
-                href="https://github.com/j0hanz/j0hanz-portfolio"
-                rel="noopener noreferrer"
-                target="_blank"
-                aria-label="Source Code"
+                <a
+                  className="text-light me-4"
+                  href="https://github.com/j0hanz"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  aria-label="GitHub Profile"
+                >
+                  <FontAwesomeIcon icon={faGithub} size="lg" />
+                </a>
+              </OverlayTrigger>
+              <OverlayTrigger
+                placement="bottom"
+                overlay={<Tooltip id="tooltip-pdf">Download CV</Tooltip>}
               >
-                <FontAwesomeIcon icon={faCode} size="lg" />
-              </a>
-            </OverlayTrigger>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  </footer>
-);
+                <a
+                  className="text-light me-4"
+                  onClick={handleModalOpen}
+                  style={{ cursor: 'pointer' }}
+                  aria-label="Download CV"
+                >
+                  <FontAwesomeIcon icon={faFilePdf} size="lg" />
+                </a>
+              </OverlayTrigger>
+              <OverlayTrigger
+                placement="bottom"
+                overlay={
+                  <Tooltip id="tooltip-certificate">Certificate</Tooltip>
+                }
+              >
+                <a
+                  className="text-light me-4"
+                  href="https://www.credential.net/dd705ce7-f66c-456a-b07d-e8712cd7287c#gs.cubcle"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  aria-label="Certificate"
+                >
+                  <FontAwesomeIcon icon={faAward} size="lg" />
+                </a>
+              </OverlayTrigger>
+              <OverlayTrigger
+                placement="bottom"
+                overlay={<Tooltip id="tooltip-code">Source Code</Tooltip>}
+              >
+                <a
+                  className="text-light"
+                  href="https://github.com/j0hanz/j0hanz-portfolio"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  aria-label="Source Code"
+                >
+                  <FontAwesomeIcon icon={faCode} size="lg" />
+                </a>
+              </OverlayTrigger>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <ModalCv show={showModal} handleClose={handleModalClose} />
+    </footer>
+  );
+};
 
 export default Footer;
