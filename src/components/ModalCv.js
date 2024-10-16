7bafa2270ad3293715b53711b83290f92613fd35
Linus Johansson <golfarenlj@gmail.com>
2024-10-16 17:50:00 +0000

    Improve styling

--- src/components/ModalCv.js
+++ src/components/ModalCv.js
@@ -1,7 +1,11 @@
 import React, { useCallback } from 'react';
 import { Modal, Button, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilePdf, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFilePdf,
+  faChevronRight,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons'; // Import faTimes
 import Cv_se from '../assets/Linus_Johansson_CV_sv.pdf';
 import Cv_en from '../assets/Linus_Johansson_CV_en.pdf';
 import styles from './styles/ModalCv.module.css';
@@ -16,25 +20,23 @@ const ModalCv = ({ show, handleClose }) => {
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
-      handleClose(); // Closes the modal after download
+      handleClose();
     },
     [handleClose]
   );
 
   return (
     <Modal show={show} onHide={handleClose} centered>
-      <Modal.Header
-        closeButton
-        closeVariant="white"
-        className="bg-dark text-light border-0"
-      />
-      <Modal.Body className="bg-dark text-light p-0">
+      <Modal.Body className={`text-light p-4 ${styles.modalBody}`}>
+        <button className={styles.closeButton} onClick={handleClose}>
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
         <Container className="text-center">
           <h4 className="mb-4">Choose Language</h4>
           <p>
             Please select the language version of the CV you'd like to download:
           </p>
-          <div className="d-flex flex-column align-items-center my-4">
+          <div className="d-flex flex-column align-items-center mt-4">
             <Button
               onClick={() => handleDownload(Cv_se, 'Linus_Johansson_CV_sv.pdf')}
               variant="outline-light"
