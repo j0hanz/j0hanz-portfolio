f6d0808ce3ed3a9537bbfc117096381ce60f14da
Linus Johansson <golfarenlj@gmail.com>
2024-10-18 01:48:39 +0000

    Refactor NavBar toggle styling to use new icon and adjust border color

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -94,6 +94,7 @@ const NavBar = () => {
           >
             <FontAwesomeIcon
               icon={faBarsStaggered}
+              size="lg"
               className={styles.navIconToggle}
             />
           </Navbar.Toggle>
