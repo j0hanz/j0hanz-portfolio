33a957a66a24e8f6995cea6341a773f8cf233a01
Linus Johansson <golfarenlj@gmail.com>
2024-10-18 13:20:04 +0000

    Remove unused useCallback import

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
