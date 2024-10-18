f28676b037ba83d3a3d46dd64850b9aedf4bdc04
Linus Johansson <golfarenlj@gmail.com>
2024-10-18 13:47:44 +0000

    Refactor Spinner component to use Bootstrap Spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Spinner as BootstrapSpinner } from 'react-bootstrap';
 import styles from './styles/Spinner.module.css';
 
 // Functional component Spinner
-const Spinner = () => {
+const Spinner = ({ size = '6rem', thickness = '0.35rem' }) => {
   return (
-    <div className={styles.spinner}>
-      <div className={styles.bounce}></div>
-      <div className={styles.bounce}></div>
+    <div className={styles.spinnerContainer}>
+      <BootstrapSpinner
+        animation="border"
+        role="status"
+        style={{
+          width: size,
+          height: size,
+          borderWidth: thickness,
+        }}
+        className={styles.spinner}
+      >
+        <span className="visually-hidden">Loading...</span>
+      </BootstrapSpinner>
     </div>
   );
 };
