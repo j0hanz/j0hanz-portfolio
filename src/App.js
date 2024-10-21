04615e921a6ca47a3d9b16b7803d053456527250
Linus Johansson <golfarenlj@gmail.com>
2024-10-21 11:50:05 +0000

    Refactor App component structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import AboutMe from './components/AboutMe';
@@ -9,7 +8,6 @@ import Education from './components/Education';
 import Portfolio from './components/Portfolio';
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
-import SuccessMessage from './components/SuccessMessage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './App.module.css';
 import Spinner from './components/Spinner';
@@ -22,41 +20,31 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000); // Loading time of 2 seconds
+    }, 2000);
 
-    return () => clearTimeout(timer); // Clean up the timer on component unmount
+    return () => clearTimeout(timer);
   }, []);
 
   return (
-    <Router>
-      <div className={styles.appContainer}>
-        <NavBar />
-        <main className={styles.mainContent}>
-          {loading ? (
-            <Spinner />
-          ) : (
-            <Routes>
-              <Route path="/success" element={<SuccessMessage />} />
-              <Route
-                path="/"
-                element={
-                  <>
-                    <Hero />
-                    <AboutMe />
-                    <Education />
-                    <Skills />
-                    <WorkExperience />
-                    <Portfolio />
-                    <ContactForm />
-                    <Footer />
-                  </>
-                }
-              />
-            </Routes>
-          )}
-        </main>
-      </div>
-    </Router>
+    <div className={styles.appContainer}>
+      <NavBar />
+      <main className={styles.mainContent}>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <Hero />
+            <AboutMe />
+            <Education />
+            <Skills />
+            <WorkExperience />
+            <Portfolio />
+            <ContactForm />
+            <Footer />
+          </>
+        )}
+      </main>
+    </div>
   );
 };
 
