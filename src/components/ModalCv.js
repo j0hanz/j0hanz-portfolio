17682523a6d8985937f65d537382436f3eaee49f
Linus Johansson <golfarenlj@gmail.com>
2024-10-21 23:53:53 +0000

    Refactor ModalCv component CSS to adjust padding, font size, and background gradient

--- src/components/ModalCv.js
+++ src/components/ModalCv.js
@@ -1,14 +1,10 @@
 import React, { useCallback } from 'react';
-import { Modal, Button, Container } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilePdf, faTimes } from '@fortawesome/free-solid-svg-icons'; // Import faTimes
+import { Modal, Container } from 'react-bootstrap';
 import Cv_se from '../assets/Linus_Johansson_CV_sv.pdf';
 import Cv_en from '../assets/Linus_Johansson_CV_en.pdf';
 import styles from './styles/ModalCv.module.css';
-import appStyles from '../App.module.css';
 
 const ModalCv = ({ show, handleClose }) => {
-  /* Handles the download of the selected CV by creating a link element and triggering the download */
   const handleDownload = useCallback(
     (cv, fileName) => {
       const link = document.createElement('a');
@@ -23,37 +19,21 @@ const ModalCv = ({ show, handleClose }) => {
   );
 
   return (
-    <Modal show={show} onHide={handleClose} centered>
-      <Modal.Body className={appStyles.modalBody}>
-        <Button className={appStyles.closeButton} onClick={handleClose}>
-          <FontAwesomeIcon icon={faTimes} />
-        </Button>
+    <Modal show={show} onHide={handleClose} centered className={styles.modalCv}>
+      <Modal.Body className={styles.modalCvBody}>
         <Container className="text-center">
-          <h4 className="mb-4">Choose Language</h4>
-          <p>
-            Please select the language version of the CV you'd like to download:
-          </p>
-          <div className="d-flex flex-column align-items-center mt-4">
-            <Button
+          <h4 className={`${styles.modalCvTitle} mb-4`}>Choose Language</h4>
+          <div
+            className={`d-flex justify-content-between ${styles.flagContainer}`}
+          >
+            <span
+              className={`fi fi-se ${styles.flagIcon}`}
               onClick={() => handleDownload(Cv_se, 'Linus_Johansson_CV_sv.pdf')}
-              className={`mb-3 ${appStyles.customButtonLight} ${styles.cvButton}`}
-            >
-              <FontAwesomeIcon
-                icon={faFilePdf}
-                className={appStyles.buttonIcon}
-              />
-              <span className={appStyles.buttonText}>Swedish CV</span>
-            </Button>
-            <Button
+            ></span>
+            <span
+              className={`fi fi-gb ${styles.flagIcon}`}
               onClick={() => handleDownload(Cv_en, 'Linus_Johansson_CV_en.pdf')}
-              className={`${appStyles.customButtonLight} ${styles.cvButton}`}
-            >
-              <FontAwesomeIcon
-                icon={faFilePdf}
-                className={appStyles.buttonIcon}
-              />
-              <span className={appStyles.buttonText}>English CV</span>
-            </Button>
+            ></span>
           </div>
         </Container>
       </Modal.Body>
