83bacb255b19a6a3030faa3b011d48c9470287c8
Linus Johansson <golfarenlj@gmail.com>
2024-10-27 13:04:27 +0000

    Initialize EmailJS with the user ID

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import emailjs from 'emailjs-com';
+
+// Initialize EmailJS with the user ID
+emailjs.init(process.env.REACT_APP_USER_ID);
 
 /* Create the root element for the React app */
 const root = ReactDOM.createRoot(document.getElementById('root'));
