7823468e31c967054e3d5fba636e686c87bf0970
Linus Johansson <golfarenlj@gmail.com>
2024-10-27 13:04:44 +0000

    Add Toast component

--- src/App.js
+++ src/App.js
@@ -11,8 +11,10 @@ import Footer from './components/Footer';
 import Badge from './components/Badge';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '/node_modules/flag-icons/css/flag-icons.min.css';
+import './toastify.css';
 import styles from './App.module.css';
 import Spinner from './components/Spinner';
+import Toast from './components/Toast';
 
 const App = () => {
   /* State to manage the loading status */
@@ -30,6 +32,7 @@ const App = () => {
   return (
     <div className={styles.appContainer}>
       <NavBar />
+      <Toast />
       <main className={styles.mainContent}>
         {loading ? (
           <Spinner />
