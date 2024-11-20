9e8dbd8307b1f970197fb03e274747b0f7d70daa
Linus Johansson <golfarenlj@gmail.com>
2024-11-20 12:48:47 +0000

    Upload and view new badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -4,7 +4,7 @@ import styles from './styles/Badge.module.css';
 
 const Badge = () => (
   <Row className="d-flex justify-content-center align-items-center">
-    <Col md={8}>
+    <Col xs={'auto'}>
       <blockquote className={styles.badge}>
         <a
           href="https://api.eu.badgr.io/public/assertions/pv52CsVuSI2V_KIyzgiahA"
@@ -19,6 +19,28 @@ const Badge = () => (
         <div className={styles.badgeText}>
           <strong className="d-block">Awarded:</strong>
           <span className={styles.badgeDate}> 25 sep. 2024</span>
+        </div>
+        <script
+          async
+          src="https://eu.badgr.com/assets/widgets.bundle.js"
+        ></script>
+      </blockquote>
+    </Col>
+    <Col xs={'auto'}>
+      <blockquote className={styles.badge}>
+        <a
+          href="https://api.eu.badgr.io/public/assertions/pO3q7BfdQFyGCP_gpMZb1A"
+          target="_blank"
+        >
+          <img
+            className={styles.badgeImage}
+            src="https://api.eu.badgr.io/public/assertions/pO3q7BfdQFyGCP_gpMZb1A/image"
+            alt="badge"
+          />
+        </a>
+        <div className={styles.badgeText}>
+          <strong className="d-block">Awarded:</strong>
+          <span className={styles.badgeDate}> 20 nov. 2024</span>
         </div>
         <script
           async
