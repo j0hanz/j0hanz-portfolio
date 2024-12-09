91da5762236e7cce7fcbf6c143cf51846e786c77
Linus Johansson <golfarenlj@gmail.com>
2024-12-09 00:44:16 +0000

    Improve structure and update styles for improved responsiveness

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -2,52 +2,30 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import styles from './styles/Badge.module.css';
 
+const BadgeItem = ({ href, imgSrc, date }) => (
+  <Col xs={'auto'}>
+    <a href={href} target="_blank">
+      <img className={styles.badgeImage} src={imgSrc} alt="badge" />
+    </a>
+    <div className={styles.badgeText}>
+      <strong className="d-block">Awarded:</strong>
+      <span className={styles.badgeDate}>{date}</span>
+    </div>
+  </Col>
+);
+
 const Badge = () => (
-  <Row className="d-flex justify-content-center align-items-center">
-    <Col xs={'auto'}>
-      <blockquote className={styles.badge}>
-        <a
-          href="https://api.eu.badgr.io/public/assertions/pv52CsVuSI2V_KIyzgiahA"
-          target="_blank"
-        >
-          <img
-            className={styles.badgeImage}
-            src="https://api.eu.badgr.io/public/assertions/pv52CsVuSI2V_KIyzgiahA/image"
-            alt="badge"
-          />
-        </a>
-        <div className={styles.badgeText}>
-          <strong className="d-block">Awarded:</strong>
-          <span className={styles.badgeDate}> 25 sep. 2024</span>
-        </div>
-        <script
-          async
-          src="https://eu.badgr.com/assets/widgets.bundle.js"
-        ></script>
-      </blockquote>
-    </Col>
-    <Col xs={'auto'}>
-      <blockquote className={styles.badge}>
-        <a
-          href="https://api.eu.badgr.io/public/assertions/pO3q7BfdQFyGCP_gpMZb1A"
-          target="_blank"
-        >
-          <img
-            className={styles.badgeImage}
-            src="https://api.eu.badgr.io/public/assertions/pO3q7BfdQFyGCP_gpMZb1A/image"
-            alt="badge"
-          />
-        </a>
-        <div className={styles.badgeText}>
-          <strong className="d-block">Awarded:</strong>
-          <span className={styles.badgeDate}> 20 nov. 2024</span>
-        </div>
-        <script
-          async
-          src="https://eu.badgr.com/assets/widgets.bundle.js"
-        ></script>
-      </blockquote>
-    </Col>
+  <Row className="d-flex justify-content-between text-center p-1 mt-5">
+    <BadgeItem
+      href="https://api.eu.badgr.io/public/assertions/pv52CsVuSI2V_KIyzgiahA"
+      imgSrc="https://api.eu.badgr.io/public/assertions/pv52CsVuSI2V_KIyzgiahA/image"
+      date="25 sep. 2024"
+    />
+    <BadgeItem
+      href="https://api.eu.badgr.io/public/assertions/pO3q7BfdQFyGCP_gpMZb1A"
+      imgSrc="https://api.eu.badgr.io/public/assertions/pO3q7BfdQFyGCP_gpMZb1A/image"
+      date="20 nov. 2024"
+    />
   </Row>
 );
 
