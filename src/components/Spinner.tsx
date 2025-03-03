2c1c5dce10bd58a083ceb66c39e77ea73b1abede
j0hanz <golfarenlj@gmail.com>
2025-03-03 23:03:36 +0100

    Fix JSX syntax for self-closing loader

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -6,7 +6,7 @@ const Spinner: FC = () => {
   return (
     <div className={styles.spinnerContainer}>
       <div className={styles.spinner}>
-        <div className={styles.loader}></div>
+        <div className={styles.loader} />
       </div>
     </div>
   );
