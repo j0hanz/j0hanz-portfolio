2f4278ec6a94e7e7a0e8fd39b96b63feaf435f00
j0hanz <golfarenlj@gmail.com>
2025-12-07 00:15:06 +0100

    refactor: remove StarBorder export from animation components

--- src/components/animations/index.ts
+++ src/components/animations/index.ts
@@ -4,5 +4,4 @@ export { FadeContent } from './FadeContent';
 export { GlitchText } from './GlitchText';
 export { ShinyText } from './ShinyText';
 export { SplitText } from './SplitText';
-export { StarBorder } from './StarBorder';
 export { TextType } from './TextType';
